969b513b146c9293e76711189b2b09ca30dd11b1
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-09-19 08:17:59 -0300

    query params

--- stage-08/api/src/server.js
+++ stage-08/api/src/server.js
@@ -11,6 +11,13 @@ app.get('/message/:id/:user', (request, response) => {
     `);
 });
 
+app.get('/users', (request, response) => {
+  const { page, limit } = request.query;
+
+  response.send(`Page: ${page}. Show: ${limit}`);
+
+});
+
 const PORT = 3333;
 
 app.listen(PORT, () => {
