149c03e99bffc060afaba937dc669a9f127ed9da
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-09-19 09:58:15 -0300

    body params

--- stage-08/api/src/server.js
+++ stage-08/api/src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.post('/users', (request, response) => {
+  const { name, email, password } = request.body;
 
-  response.send(`You called the post!`);
+  response.json({ name, email, password });
 
 });
 
