b3dc075c0641f6dff96e4ca0ce5f0fe13c1701d1
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-09-25 10:04:58 -0300

    wherein

--- stage-08/api/src/controllers/NotesController.js
+++ stage-08/api/src/controllers/NotesController.js
@@ -56,11 +56,22 @@ class NotesController {
   }
 
   async index(request, response) {
-    const { user_id } = request.query;
+    const { user_id, title, tags } = request.query;
 
-    const notes = await knex("notes")
+    let notes;
+
+    if (tags) {
+      const filteredTags = tags.split(',').map(tag => tag.trim());
+
+      notes = await knex("tags")
+        .whereIn("name", filteredTags)
+
+    } else {
+      notes = await knex("notes")
       .where({ user_id })
+      .whereLike("title", `%${title}%`)
       .orderBy("title");
+    }
 
     return response.json(notes);
   }
