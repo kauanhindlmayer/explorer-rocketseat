cb8475988c01d144685a8c749eac884b11807681
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-09-26 19:47:24 -0300

    header component

--- stage-09-10/rocketnotes/src/pages/Details/index.jsx
+++ stage-09-10/rocketnotes/src/pages/Details/index.jsx
@@ -2,13 +2,13 @@ import { Container } from "./styles"
 
 import { Button } from "../../components/Button"
 
+import { Header } from "../../components/Header"
+
 export function Details() {
   return (
     <Container>
-      <h1>Hello, World!</h1>
-      <Button title="Button 1" loading />
-      <Button title="Button 2" />
-      <Button title="Button 3" />
+      <Header />
+      <Button title="Back" />
     </Container>
   )
 }
