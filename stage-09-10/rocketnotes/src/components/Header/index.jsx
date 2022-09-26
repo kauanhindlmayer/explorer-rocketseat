384b088966eb321bb4464ccfa4d33c8370cda553
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-09-26 20:03:59 -0300

    user image

--- stage-09-10/rocketnotes/src/components/Header/index.jsx
+++ stage-09-10/rocketnotes/src/components/Header/index.jsx
@@ -1,9 +1,20 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header() {
   return (
     <Container>
+      <Profile>
+        <img 
+          src="http://github.com/kauanhindlmayer.png"
+          alt="user photo"
+        />
 
+        <div>
+          <span>Welcome</span>
+          <strong>Kauan Hindlmayer</strong>
+        </div>        
+
+      </Profile>
     </Container>
   );
 }
