7bf1bae9114f6630e122a4b420fe160ddde2dd8a
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-09-26 19:37:21 -0300

    props

--- stage-09-10/rocketnotes/src/pages/Details/index.jsx
+++ stage-09-10/rocketnotes/src/pages/Details/index.jsx
@@ -6,7 +6,9 @@ export function Details() {
   return (
     <Container>
       <h1>Hello, World!</h1>
-      <Button />
+      <Button title="Button 1" loading />
+      <Button title="Button 2" />
+      <Button title="Button 3" />
     </Container>
   )
 }
