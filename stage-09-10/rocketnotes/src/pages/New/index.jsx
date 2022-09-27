5a2ab0bb545f9df2bf9ae0d8195bed0947c4896b
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-09-27 17:57:40 -0300

    new component

---
+++ stage-09-10/rocketnotes/src/pages/New/index.jsx
@@ -0,0 +1,26 @@
+import { Container, Form } from './styles';
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { Textarea } from '../../components/Textarea';
+
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Create note</h1>
+            <a href="/">back</a>
+          </header>
+
+          <Input placeholder="Title" />
+
+          <Textarea placeholder="Comments" />
+        </Form>
+      </main>
+    </Container>
+  );
+}
