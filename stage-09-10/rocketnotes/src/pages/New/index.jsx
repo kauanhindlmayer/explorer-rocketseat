8bfeee90ef31e0d13ac6a69ae92dce9f96646a92
Kauan <kauanhindlmayer07@gmail.com>
2022-09-27 19:03:28 -0300

    noteItem component

--- stage-09-10/rocketnotes/src/pages/New/index.jsx
+++ stage-09-10/rocketnotes/src/pages/New/index.jsx
@@ -2,7 +2,9 @@ import { Container, Form } from './styles';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea';
-
+import { NoteItem } from '../../components/NoteItem';
+import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
 
 export function New() {
   return (
@@ -19,6 +21,20 @@ export function New() {
           <Input placeholder="Title" />
 
           <Textarea placeholder="Comments" />
+
+          <Section title="Useful links">
+            <NoteItem value="https://rocketseat.com.br" />
+            <NoteItem isNew placeholder="New link" />
+          </Section>
+
+          <Section title="Tags">
+            <div className="tags">
+              <NoteItem value="react" />
+              <NoteItem isNew placeholder="New tag" />
+            </div>
+          </Section>
+
+          <Button title="Save" />
         </Form>
       </main>
     </Container>
