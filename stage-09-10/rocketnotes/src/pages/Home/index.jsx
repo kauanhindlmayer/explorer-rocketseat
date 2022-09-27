f4f1a7deae8df8a02c3cbe66118ade6978f66e73
Kauan <kauanhindlmayer07@gmail.com>
2022-09-27 10:38:55 -0300

    note component

--- stage-09-10/rocketnotes/src/pages/Home/index.jsx
+++ stage-09-10/rocketnotes/src/pages/Home/index.jsx
@@ -3,7 +3,9 @@ import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
 import { Header } from '../../components/Header';
 import { ButtonText } from '../../components/ButtonText';
+import { Note } from '../../components/Note';
 import { Input } from '../../components/Input';
+import { Section } from '../../components/Section';
 
 export function Home() {
   return (
@@ -31,7 +33,15 @@ export function Home() {
       </Search>
 
       <Content>
-
+        <Section title="My notes">
+          <Note data={{
+            title: 'React Modal', 
+            tags: [
+              {id: '1', name: 'react'},
+              {id: '2', name: 'components'}
+            ]
+          }} />
+        </Section>
       </Content>
 
       <NewNote>
