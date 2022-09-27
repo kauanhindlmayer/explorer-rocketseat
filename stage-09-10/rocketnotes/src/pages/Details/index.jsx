85bd8aefaf0fb0ee353cfab5a1f388c9560b1c9c
Kauan <kauanhindlmayer07@gmail.com>
2022-09-27 07:59:23 -0300

    buttonText component

--- stage-09-10/rocketnotes/src/pages/Details/index.jsx
+++ stage-09-10/rocketnotes/src/pages/Details/index.jsx
@@ -4,11 +4,15 @@ import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
 import { Section } from "../../components/Section"
 import { Tag } from "../../components/Tag"
+import { ButtonText } from "../../components/ButtonText"
 
 export function Details() {
   return (
     <Container>
       <Header />
+
+      <ButtonText title="Delete note" />
+
       <Section title="Useful links">
         <Links>
           <li>
