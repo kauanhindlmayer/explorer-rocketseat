ed1196f07ebc827bf2184af1d8b02e176a49c8a6
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-09-28 15:16:35 -0300

    serving files

--- stage-09-10/api/src/server.js
+++ stage-09-10/api/src/server.js
@@ -1,17 +1,18 @@
 require("express-async-errors");
 
 const migrationRuns = require("./database/sqlite/migrations");
-
 const AppError = require("./utils/AppError");
+const uploadConfig = require("./configs/upload");
 
 const express = require("express");
-
 const routes = require("./routes");
 
 migrationRuns();
 
 const app = express();
 app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 
