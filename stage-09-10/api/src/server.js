dd1ce92ff804c4bd55f2c2836515aeccdb8a2017
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-09-28 20:18:19 -0300

    setting axios

--- stage-09-10/api/src/server.js
+++ stage-09-10/api/src/server.js
@@ -4,12 +4,14 @@ const migrationRuns = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const uploadConfig = require("./configs/upload");
 
+const cors = require("cors");
 const express = require("express");
 const routes = require("./routes");
 
 migrationRuns();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
