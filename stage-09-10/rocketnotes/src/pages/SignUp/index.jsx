b060c97d9aba1fbbd58215f4c7cfe9765c3bb4ab
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-09-28 20:45:00 -0300

    registering user

--- stage-09-10/rocketnotes/src/pages/SignUp/index.jsx
+++ stage-09-10/rocketnotes/src/pages/SignUp/index.jsx
@@ -1,12 +1,39 @@
+import { useState } from 'react';
 import { Container, Form, Background } from './styles';
 import { FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Link, useNavigate } from 'react-router-dom';
 
-import { Link } from 'react-router-dom';
+import { api } from "../../services/api";
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert("Fill in all fields!");
+    }
+
+    api.post("/users", { name, email, password })
+      .then(() => {
+        alert("User registered successfully!");
+        navigate("/");
+      })
+      .catch(error => {
+        if (error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("Could not register!");
+        }
+      })
+  }
+
   return (
     <Container>
       <Background />
@@ -21,21 +48,24 @@ export function SignUp() {
           placeholder="Name"
           type="text"
           icon={FiUser}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input 
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
           placeholder="Password"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Register" />
+        <Button title="Register" onClick={handleSignUp} />
 
         <Link to="/">Back to Login</Link>
 
