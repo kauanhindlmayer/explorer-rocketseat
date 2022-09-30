3fc59f9c73226d865274247b4d37dea7334c033f
Kauan <kauanhindlmayer07@gmail.com>
2022-09-29 22:00:47 -0300

    creating context

--- stage-09-10/rocketnotes/src/main.jsx
+++ stage-09-10/rocketnotes/src/main.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
-import theme from './styles/theme'
 
+import { MyContext } from './myContext'
+import { ThemeProvider } from 'styled-components'
 import { Routes } from './routes'
+import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes />
+      <MyContext.Provider value={{ email: 'kauan@email.com'}} >
+        <Routes />
+      </MyContext.Provider>
     </ThemeProvider>
   </React.StrictMode>
 )
