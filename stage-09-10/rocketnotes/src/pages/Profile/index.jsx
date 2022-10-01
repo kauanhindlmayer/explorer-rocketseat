8aca6b53d796961cbc9503d6251d327043c51c29
Kauan <kauanhindlmayer07@gmail.com>
2022-10-01 10:41:57 -0300

    loading user data

--- stage-09-10/rocketnotes/src/pages/Profile/index.jsx
+++ stage-09-10/rocketnotes/src/pages/Profile/index.jsx
@@ -1,12 +1,22 @@
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
 
+import { useState } from 'react';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import { Link } from 'react-router-dom';
 
+import { useAuth } from '../../hooks/auth';
+
 import { Container, Form, Avatar } from "./styles";
 
 export function Profile() {
+  const { user } = useAuth();
+
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [passwordNew, setPasswordNew] = useState();
+  const [passwordOld, setPasswordOld] = useState();
+
   return (
     <Container>
       <header>
@@ -35,24 +45,30 @@ export function Profile() {
           placeholder="Name"
           type="text"
           icon={FiUser}
+          value={name}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input 
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          value={email}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
           placeholder="Password"
           type="password"
           icon={FiLock}
+          onChange={e => setPasswordOld(e.target.value)}
         />
 
         <Input 
           placeholder="New Password"
           type="password"
           icon={FiLock}
+          onChange={e => setPasswordNew(e.target.value)}
         />
 
         <Button 
