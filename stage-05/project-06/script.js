c1f2bac2472750da7b0f692d999b555c38731f26
Kauan <kauanhindlmayer07@gmail.com>
2022-10-31 20:39:25 -0300

    Estruturando os dados do Modal

--- stage-05/project-06/script.js
+++ stage-05/project-06/script.js
@@ -3,10 +3,19 @@ const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
 const inputHeight = document.querySelector('#height');
 
-const modalWrapper = document.querySelector('.modal-wrapper');
-const modalMessage = document.querySelector('.modal .title span');
-const modalBtnClose = document.querySelector('.modal button.close');
+const Modal = {
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .title span'),
+  buttonClose: document.querySelector('.modal button.close'),
 
+  open() {
+    Modal.wrapper.classList.add('open');
+  },
+
+  close() {
+    Modal.wrapper.classList.remove('open');
+  }
+}
 
 form.onsubmit = (event) => {
   event.preventDefault();
@@ -17,11 +26,11 @@ form.onsubmit = (event) => {
   const result = IMC(weight, height);
   const message = `Seu IMC é de ${result}`;
   
-  modalWrapper.classList.add('open');
-  modalMessage.innerText = message;
+  Modal.open();
+  Modal.message.innerText = message;
 }
 
-modalBtnClose.onclick = () => modalWrapper.classList.remove('open');
+Modal.buttonClose.onclick = () => Modal.close();
 
 const IMC = (weight, height) => {
   return (weight / ((height / 100) ** 2)).toFixed(2);
