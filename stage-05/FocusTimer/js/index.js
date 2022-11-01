8fd3fe9e5d73dbc0352b793a83935493d74eb237
Kauan <kauanhindlmayer07@gmail.com>
2022-11-01 19:56:38 -0300

    Refatorando para novas funcionalidades

--- stage-05/FocusTimer/js/index.js
+++ stage-05/FocusTimer/js/index.js
@@ -1,14 +1,38 @@
-let play = document.querySelector('.play');
-let pause = document.querySelector('.pause');
+const buttonPlay = document.querySelector('.play');
+const buttonPause = document.querySelector('.pause');
+const buttonStop = document.querySelector('.stop');
+const buttonSet = document.querySelector('.set');
+const buttonSoundOn = document.querySelector('.sound-on');
+const buttonSoundOff = document.querySelector('.sound-off');
 
 // Programação imperativa
-//                    event    callback function
-play.addEventListener('click', () => {
-  play.classList.add('hide');
-  pause.classList.remove('hide');
+//                          event    callback function
+buttonPlay.addEventListener('click', () => {
+  buttonPlay.classList.add('hide');
+  buttonPause.classList.remove('hide');
+  buttonSet.classList.add('hide');
+  buttonStop.classList.remove('hide');
 });
 
-pause.addEventListener('click', () => {
-  play.classList.remove('hide');
-  pause.classList.add('hide');  
+//                          event    callback function
+buttonPause.addEventListener('click', () => {
+  buttonPlay.classList.remove('hide');
+  buttonPause.classList.add('hide');  
+});
+
+buttonStop.addEventListener('click', () => {
+  buttonPlay.classList.remove('hide');
+  buttonPause.classList.add('hide');
+  buttonSet.classList.remove('hide');
+  buttonStop.classList.add('hide');
+});
+
+buttonSoundOff.addEventListener('click', () => {
+  buttonSoundOn.classList.remove('hide');
+  buttonSoundOff.classList.add('hide');
+});
+
+buttonSoundOn.addEventListener('click', () => {
+  buttonSoundOn.classList.add('hide');
+  buttonSoundOff.classList.remove('hide');
 });
